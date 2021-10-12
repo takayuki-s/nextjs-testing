f549b8634bb1d1f16cb004ef7343ed68fe5d3b23
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-13 06:33:01 +0900

    BlogPageのrenderテスト（2fails）

--- __tests__/BlogPage.test.tsx
+++ __tests__/BlogPage.test.tsx
@@ -10,3 +10,42 @@ import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
 initTestHelpers()
+
+const handlers = [
+  rest.get(
+    'https://jsonplaceholder.typicode.com/posts/?_limit=10',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          { userId: 1, id: 1, title: 'dummy title 1', body: 'dummy body 1' },
+          { userId: 2, id: 2, title: 'dummy title 2', body: 'dummy body 2' },
+        ])
+      )
+    }
+  ),
+]
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+// テスト間の副作用を防ぐ
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Blog page', () => {
+  it('Should render the list of blogs pre-fetched by getStaticProps', async () => {
+    const { page } = await getPage({
+      route: 'blog-page',
+    })
+    render(page)
+    expect(await screen.findByText('blog page')).toBeInTheDocument()
+    expect(screen.getByText('dummy title 1')).toBeInTheDocument()
+    expect(screen.getByText('dummy title 2')).toBeInTheDocument()
+  })
+})
