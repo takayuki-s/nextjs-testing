376f8efe770720aed8ad4684901e232f7c36d805
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-15 06:51:41 +0900

    commentデータをAPI取得
    const fetcher = axios.get(
        'API取得するURL'
      ).then(res => http://res.data)
    const { data, error } = useSWR('/api/data', fetcher)
    
    第1引数はkeyとなるURLが多く、第2引数でfetch処理をする関数を呼び出す。上はaxiosでfetchしたデータを第2引数にするパターン

--- pages/comment-page.tsx
+++ pages/comment-page.tsx
@@ -1,6 +1,21 @@
 import Layout from '../components/Layout'
+import useSWR from 'swr'
+import axios from 'axios'
+import Comment from '../components/Comment'
+import { COMMENT } from '../types/Types'
+
+const axiosFetcher = async () => {
+  const result = await axios.get<COMMENT[]>(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10'
+  )
+  return result.data
+}
 
 const CommentPage: React.FC = () => {
+  const { data: comments, error } = useSWR('commentsFetch', axiosFetcher)
+
+  if (error) return <span>Error!</span>
+
   return (
     <Layout title="Comment">
       <p className="text-4xl">comment page</p>
