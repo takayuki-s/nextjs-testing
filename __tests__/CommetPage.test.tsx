d94f02340e838f0ba29738ce92e05dadde2089e9
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-16 09:30:36 +0900

    モックサーバー

--- __tests__/CommetPage.test.tsx
+++ __tests__/CommetPage.test.tsx
@@ -8,3 +8,36 @@ import { SWRConfig } from 'swr'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import CommentPage from '../pages/comment-page'
+
+const server = setupServer(
+  rest.get(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10',
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            postId: 1,
+            id: 1,
+            name: 'A',
+            email: 'dummya@gmail.com',
+            body: 'test body a',
+          },
+          {
+            postId: 2,
+            id: 2,
+            name: 'B',
+            email: 'dummyb@gmail.com',
+            body: 'test body b',
+          },
+        ])
+      )
+    }
+  )
+)
+beforeAll(() => server.listen())
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => server.close())
